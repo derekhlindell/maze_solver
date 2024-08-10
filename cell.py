2519d6c57c5aab6f3a511ad2f25b56d74e6363e5
derekhlindell <derek.h.lindell@gmail.com>
2024-08-10 16:12:27 -0700

    created cell.py and modified main to use it

---
+++ cell.py
@@ -0,0 +1,53 @@
+from graphics import Window, Line, Point
+
+
+class Cell:
+    def __init__(self, win):
+        self.has_left_wall = True
+        self.has_right_wall = True
+        self.has_top_wall = True
+        self.has_bottom_wall = True
+        self.__x1 = None
+        self.__y1 = None
+        self.__x2 = None
+        self.__y2 = None
+        self.__win = win
+    
+    def draw(self, x1, y1, x2, y2):
+        self.__x1 = x1
+        self.__y1 = y1
+        self.__x2 = x2
+        self.__y2 = y2
+        
+        if self.has_left_wall:
+            self.__win.draw_line(
+                Line(
+                    Point(x1, y1), 
+                    Point(x1, y2)
+                ), 
+                "black"
+            )
+        if self.has_right_wall:
+            self.__win.draw_line(
+                Line(
+                    Point(x2, y1), 
+                    Point(x2, y2)
+                ), 
+                "red"
+            )
+        if self.has_top_wall:
+            self.__win.draw_line(
+                Line(
+                    Point(x1, y1), 
+                    Point(x2, y1)
+                ), 
+                "blue"
+            )
+        if self.has_bottom_wall:
+            self.__win.draw_line(
+                Line(
+                    Point(x1, y2), 
+                    Point(x2, y2)
+                ), 
+                "yellow"
+            )
--- graphics.py
+++ graphics.py
@@ -1,5 +1,6 @@
 from tkinter import Tk, BOTH, Canvas
 
+
 class Window:
     def __init__(self, width, height):
         self.__root = Tk()
@@ -19,12 +20,12 @@ class Window:
             self.redraw()
         print("Window has been closed.")
 
-    def draw_line(self, line, fill_color):
+    def draw_line(self, line, fill_color="black"):
+        # creates a line from an input line object with the input fill color
         line.draw(self.__canvas, fill_color)
 
     def close(self):
         self.__running = False
-
 
 
 class Point:
@@ -33,12 +34,13 @@ class Point:
         self.y = y
 
 
-
 class Line:
+    # takes in two point classes
     def __init__(self, point1, point2):
         self.__point1 = point1
         self.__point2 = point2
-
+    
+    # draws a line on the current canvas
     def draw(self, canvas, fill_color):
         canvas.create_line(
             self.__point1.x, self.__point1.y,
